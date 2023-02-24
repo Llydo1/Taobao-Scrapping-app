2f0c1e76e00bcd01c5a7a2e5816b58c9b7b239aa
Beaniss <Beaniss1357@gmail.com>
2023-02-24 23:55:14 +0700

    add GUI

--- Backend/View/GUI.py
+++ Backend/View/GUI.py
@@ -1,7 +1,99 @@
 import sys
-sys.path.insert(0, '..')
-from Taobao.Controller.Controller import Controller
+import tty
+import termios
 
-controller = Controller()
+def option1():
+    global result
+    result += "Option 1 selected\n"
+    print(result)
 
-controller.scrap_all_item("https://world.taobao.com/dianpu/142967287.htm", "uustudio")
+def option2():
+    global result
+    result += "Option 2 selected\n"
+    print(result)
+
+def option3():
+    global result
+    result += "Option 3 selected\n"
+    print(result)
+
+def option4():
+    global result
+    result += "Option 4 selected\n"
+    print(result)
+
+result = ""
+current_choice = 1
+
+def get_key():
+    """
+    Get a single character from the user without requiring them to press enter.
+    """
+    old_settings = termios.tcgetattr(sys.stdin)
+    try:
+        tty.setcbreak(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+    return ch
+
+while True:
+    print("Choose an option:")
+    print("1. Option 1")
+    print("2. Option 2")
+    print("3. Option 3")
+    print("4. Option 4")
+    print("5. Exit")
+
+    if current_choice == 1:
+        print("\033[7m1. Option 1\033[0m")
+        print("2. Option 2")
+        print("3. Option 3")
+        print("4. Option 4")
+    elif current_choice == 2:
+        print("1. Option 1")
+        print("\033[7m2. Option 2\033[0m")
+        print("3. Option 3")
+        print("4. Option 4")
+    elif current_choice == 3:
+        print("1. Option 1")
+        print("2. Option 2")
+        print("\033[7m3. Option 3\033[0m")
+        print("4. Option 4")
+    elif current_choice == 4:
+        print("1. Option 1")
+        print("2. Option 2")
+        print("3. Option 3")
+        print("\033[7m4. Option 4\033[0m")
+
+    choice = None
+    while choice is None:
+        key = get_key()
+        if key == "\033":
+            # Arrow key was pressed
+            key = get_key()
+            if key == "[":
+                # Arrow key sequence continues
+                key = get_key()
+                if key == "A":
+                    # Up arrow key
+                    current_choice = max(current_choice - 1, 1)
+                elif key == "B":
+                    # Down arrow key
+                    current_choice = min(current_choice + 1, 4)
+        elif key == "\r":
+            # Enter key
+            choice = str(current_choice)
+
+    if choice == "1":
+        option1()
+    elif choice == "2":
+        option2()
+    elif choice == "3":
+        option3()
+    elif choice == "4":
+        option4()
+    elif choice == "5":
+        break
+    else:
+        print("Invalid choice. Please try again.")
